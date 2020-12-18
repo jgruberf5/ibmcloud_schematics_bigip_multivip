afa34dd09dc10f96ccc21bc63d46872e34615484
John Gruber <jgruber@f5.com>
2020-12-17 20:55:01 -0600

    commented out incomplete resources

--- compute.tf
+++ compute.tf
@@ -155,25 +155,25 @@ locals {
 #}
 
 
-output "resource_name" {
-  value = ibm_is_instance.f5_ve_instance.name
-}
+#output "resource_name" {
+#  value = ibm_is_instance.f5_ve_instance.name
+#}
 
-output "resource_status" {
-  value = ibm_is_instance.f5_ve_instance.status
-}
+#output "resource_status" {
+#  value = ibm_is_instance.f5_ve_instance.status
+#}
 
-output "VPC" {
-  value = ibm_is_instance.f5_ve_instance.vpc
-}
+#output "VPC" {
+#  value = ibm_is_instance.f5_ve_instance.vpc
+#}
 
 output "image_id" {
   value = local.image_id
 }
 
-output "instance_id" {
-  value = ibm_is_instance.f5_ve_instance.id
-}
+#output "instance_id" {
+#  value = ibm_is_instance.f5_ve_instance.id
+#}
 
 output "profile_id" {
   value = data.ibm_is_instance_profile.instance_profile.id
@@ -187,9 +187,9 @@ output "virtual_service_addresses" {
   value = local.virtual_service_addresses
 }
 
-output "f5_shell_access" {
-  value = "ssh://root@${ibm_is_floating_ip.f5_management_floating_ip.address}"
-}
+#output "f5_shell_access" {
+#  value = "ssh://root@${ibm_is_floating_ip.f5_management_floating_ip.address}"
+#}
 
 output "f5_phone_home_url" {
   value = var.phone_home_url
